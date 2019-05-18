999e43e942e5799e84d34ea3b9ab0fe81ec3074a
Cris Duran <cris@franssonduran.se>
2019-05-18 21:16:37 +0200

    Initialize the child theme. Add the required files for a Twenty Seventeen Child Theme for the site elbaroncafe.se

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function childtheme_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', 
+    get_template_directory_uri() . '/style.css' );
+
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( 'parent-style' ),
+    wp_get_theme()->get('Version')
+  );
+}
+add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_styles' );
